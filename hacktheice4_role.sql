e8622a6b1326be72a33cc6d0d021038313b5fb9d
AgniaEndie <Endienasg2@gmail.com>
2022-11-25 16:32:42 +0900

    Changes

--- hacktheice4_role.sql
+++
@@ -1,8 +0,0 @@
-create table role
-(
-    role_id bigint unsigned auto_increment
-        primary key,
-    role    varchar(240) not null
-)
-    collate = utf8mb4_unicode_ci;
-
--- hacktheice4_status.sql
+++
@@ -1,8 +0,0 @@
-create table status
-(
-    status_id bigint unsigned auto_increment
-        primary key,
-    status    varchar(240) not null
-)
-    collate = utf8mb4_unicode_ci;
-
